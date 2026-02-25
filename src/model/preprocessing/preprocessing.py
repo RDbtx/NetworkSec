e4d5e27fa68a14019b9d60766c640cba6b5e2aea
riccardo <riccardodeidda25@gmail.com>
2026-02-25 15:44:07 +0100

    minor fix

--- src/model/preprocessing/preprocessing.py
+++ src/model/preprocessing/preprocessing.py
@@ -4,8 +4,7 @@ from labeling import labeling
 
 if __name__ == "__main__":
     print("Starting preprocessing...\n")
-    #labeling()  # adds Label to raw CSVs
-    # output = filtering()  # merges labeled CSVs, returns path
-    output = "/Users/riccardo/Desktop/NetworkSec/src/model/output/pcap-all.csv"
+    labeling()  # adds Label to raw CSVs
+    output = filtering()  # merges labeled CSVs, returns path
     scaling(output)  # scales and encodes, saves final CSV
     print("\nFinished!")
