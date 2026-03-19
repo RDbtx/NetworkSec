dc7a6fde482076d923b513ff29ea9468bcc4a6ca
riccardo <riccardodeidda25@gmail.com>
2026-03-19 23:06:39 +0100

    Updated packet extraction logic and regenerated dataset. Then benchmarked various models against the original dataset baselines. 10-Class XGB trained on the newly extracted dataset was identified as the top-performing model

--- checklabels.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("/Users/riccardo/Desktop/NetworkSec/src/model/dataset/1-http-flood/pcap1-caddy-l.csv")
-labels = sorted(df["Label"].dropna().unique())
-print(labels)
